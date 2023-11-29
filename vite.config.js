ade434a96dcefa7725f9cc5ee20da11607f4618f
Fabio Novack <fabio.m2@hotmail.com>
2023-11-29 18:29:14 -0300

    corrigindo erro package

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ import ViteFonts from 'unplugin-fonts/vite'
 
 // Utilities
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     vue({
       template: { transformAssetUrls }
     }),
